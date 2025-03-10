316219f3d53d00613db59f680116bdfed595a629
cristianp1993 <cristian.piedrahita88@gmail.com>
2025-03-10 08:50:23 -0500

    proyecto

---
+++ ProyectoU2025/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoU2025.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
