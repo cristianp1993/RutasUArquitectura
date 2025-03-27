866a747cff9960362e4d0e03f63acc3d4d72f233
cristianp1993 <cristian.piedrahita88@gmail.com>
2025-03-27 00:10:26 -0500

    Inicio Login

---
+++ ProyectoU2025/Db/DapperContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Data.Common;
+
+
+namespace ProyectoU2025.Db
+{
+    public class DapperContext : IDapperContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        // Método para crear una conexión sin abrir
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+
+        // Método asíncrono para crear y abrir una conexión
+        public async Task<SqlConnection> CreateConnectionAsync()
+        {
+            var connection = new SqlConnection(_connectionString);
+            await connection.OpenAsync();
+            return connection;
+        }
+        
+    }
+}
---
+++ ProyectoU2025/Querys/UsuariosQuerys.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoU2025.Querys
+{
+    public class UsuariosQuerys
+    {
+        public static string GetByEmail => @"
+        SELECT usu_id as Id, usu_email as Email, usu_rol as Rol 
+        FROM t_usuarios 
+        WHERE usu_email = @Email";
+    }
+}
